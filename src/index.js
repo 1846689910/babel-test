00bdc485c2e13c788a4dfaf1fc4b8d2fe82d029a
1846689910 <zuhe75@gmail.com>
2019-02-19 14:58:30 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+class Test {
+  constructor() {}
+
+  hello(height = 50) {
+    console.log("hello, world", height);
+    console.log(`hello, template ${height}`);
+  }
+}
+const x = new Test();
+x.hello();
+const fsPromises = require("fs").promises;
+(async() => {
+    const {aaa: content} = await Promise.resolve({aaa: 123});
+})();
---
+++ webpack.config.js
@@ -0,0 +1,101 @@
+const path = require("path");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+
+const {entry, jsRule} = (() => {
+    const entry = {};
+    const jsRule = {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+    };
+    if (process.env.babelEnvTargets) {
+        const oneOf = [];
+        const babelEnvTargets = JSON.parse(process.env.babelEnvTargets);
+        Object.entries(babelEnvTargets).forEach(([k, v]) => {
+            entry[k] = ["@babel/polyfill", `${__dirname}/src/index.js?${k}`];
+            oneOf.push({
+                resourceQuery: new RegExp(k),
+                use: [
+                    {
+                        loader: "babel-loader",
+                        options: {
+                            presets: [
+                                ["@babel/preset-env", {
+                                    targets: v
+                                }]
+                            ]
+                        }
+                    }
+                ]
+            });
+        });
+        jsRule.oneOf = oneOf;
+    } else {
+        entry.main = `${__dirname}/src/index.js`;
+        jsRule.use = [
+            {
+                loader: "babel-loader",
+                options: {
+                    presets: [
+                        ["@babel/preset-env"]
+                    ]
+                }
+            }
+        ];
+    }
+    return {entry, jsRule};
+})();
+
+module.exports = env => ({
+    mode: env.mode,
+    entry,
+    output: {
+        path: path.resolve(__dirname, "./dist"),
+        publicPath: "/",
+        filename: `./[name]/bundle.js`
+    },
+    module: {
+        rules: [
+            jsRule,
+            // {
+            //     test: /\.css$/,
+            //     use: [
+            //         {
+            //             loader: "style-loader"
+            //         },
+            //         {
+            //             loader: "css-loader"
+            //         }
+            //     ]
+            // },
+            // {
+            //     test: /\.(png|gif|jpg)$/,
+            //     use: [
+            //         {
+            //             loader: "file-loader",
+            //             options: {
+            //                 name: "images/[name].[ext]"
+            //             }
+            //         }
+            //     ]
+            // }
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: "ts-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin([
+            `${__dirname}/dist/*`
+        ], {
+            root: __dirname,
+            exclude: [],
+            verbose: false
+        })
+    ]
+});
