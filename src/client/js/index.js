b08fdfedbdbe349f6c85d400a254c318a0b7a97a
1846689910 <eric14706@gmail.com>
2019-09-12 00:06:04 -0700

    mod

---
+++ src/client/js/index.js
@@ -0,0 +1,9 @@
+import style from "../style/main.css";
+import logo from "../images/electrode.png";
+class Student {
+  constructor(name, age){
+    this.name = name;
+    this.age = age;
+  }
+}
+const s = new Student("Alex", 21);
--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+"use strict";
 /**
 for webpack:
 npm i webpack webpack-cli webpack-dev-server --save-dev
@@ -9,7 +10,7 @@ for babel:
 rest `@babel/*` and `babel-*` packages
 
 */
-
+const Path = require("path");
 const babelLoader = require.resolve("babel-loader");
 const cssLoader = require.resolve("css-loader");
 const postcssLoader = require.resolve("postcss-loader");
@@ -18,9 +19,20 @@ const postcssPresetEnv = require("postcss-preset-env");
 const autoprefixer = require("autoprefixer");
 const fileLoader = require.resolve("file-loader");
 const urlLoader = require.resolve("url-loader");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "production",
+  entry: { main: [Path.resolve("src/client/js/index.js")] }, // with polyfill: { main: ["core-js", "regenerator-runtime/runtime", "src/client/js/index.js"] }
+  output: {
+    path: Path.resolve("dist/js"),
+    publicPath: "/js/",
+    chunkFilename: "[hash].[name].js",
+    filename: "[name].bundle.[hash].js"
+  },
   module: {
     rules: [
       {
@@ -30,6 +42,10 @@ module.exports = {
       {
         test: /\.css/,
         use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: { publicPath: "" }
+          },
           {
             loader: cssLoader
           },
@@ -77,5 +93,52 @@ module.exports = {
         use: [fileLoader]
       }
     ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".tsx"]
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].style.[hash].css"
+    }),
+    new OptimizeCssAssetsPlugin({
+      cssProcessorOptions: {
+        zindex: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      options: {
+        context: Path.resolve("src")
+      }
+    }),
+    new CleanWebpackPlugin()
+  ],
+  optimization: {
+    splitChunks: {
+      name: process.env.NODE_ENV !== "production", //  set splitChunks.name to false for production builds so that it doesn't change names unnecessarily
+      chunks: "all", // split code in app and node_modules into bundle and vendor.bundle.js
+      maxInitialRequests: Infinity,
+      minSize: 0,
+      cacheGroups: {
+        // keep splitting the node_module chunks
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          chunks: "all",
+          name(module) {
+            const packageName = module.context.match(
+              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+            )[1];
+            return `npm.${packageName.replace("@", "")}`;
+          }
+        },
+        // includes all code shared between entry points
+        commons: {
+          name: "commons",
+          chunks: "initial",
+          minChunks: 2
+        }
+      }
+    }
   }
 };
